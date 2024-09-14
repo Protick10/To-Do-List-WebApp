b46c7ef378e1f7acd614bb497305961ce28f87d1
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-09-14 20:16:11 +0600

    register styling completed

--- register.php
+++ register.php
@@ -69,6 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
     <title>Register</title>
 </head>
 <body>
